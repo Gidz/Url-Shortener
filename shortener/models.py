884b6953a7dc057d729913637e31b03ac453ecb7
gideon <gideon@beasty.home>
2018-03-01 16:22:57 -0500

    Registered the model in the admin site

--- shortener/models.py
+++ shortener/models.py
@@ -3,4 +3,7 @@ from django.db import models
 # Create your models here.
 class URLs(models.Model):
     url = models.CharField(max_length=500)
-    shorturl = models.CharField(max_length=15, blank=True)
+    shorturl = models.CharField(max_length=15, blank=True, unique=True)
+
+    def __str__(self):
+        return self.url
