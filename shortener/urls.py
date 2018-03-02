ec20950b79d251a6f71b70e1f1bb407596fff336
gideon <gideon@beasty.home>
2018-03-02 17:25:07 -0500

    URL shortener form working

--- shortener/urls.py
+++ shortener/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('',views.index),
-    path('<slug:slug>',views.short_url_redirect)
+    path('<slug:slug>',views.short_url_redirect),
+    path('shorten/',views.shorten_url)
 ]
--- shortener/views.py
+++ shortener/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import TinyUrl
+from .utils import codegen
 
 def index(request):
     return render(request,"shortener/index.html",{})
@@ -8,3 +9,13 @@ def index(request):
 def short_url_redirect(request,slug):
     obj = TinyUrl.objects.get(shorturl=slug)
     return redirect(obj.url)
+
+def shorten_url(request):
+    if(request.method=='POST'):
+        raw_url = request.POST['raw_url']
+        TinyUrl.objects.create(
+        url = raw_url,
+        shorturl = codegen()
+        )
+        print("Tried to save it")
+    return HttpResponse("URL saved")
